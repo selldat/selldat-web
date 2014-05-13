391e6695adf694f82bd218641641941e72383ca0
wilsonbalderrama <wilson.balderrama@gmail.com>
2014-05-13 13:25:54 -0400

    Added config file for staging part

---
+++ server/config/env/staging.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+    db: process.env.MONGOHQ_URL,
+    port: process.env.PORT || 5000,
+    app: {
+        name: 'Selldat - Staging'
+    },
+    facebook: {
+        clientID: 'APP_ID',
+        clientSecret: 'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    },
+    twitter: {
+        clientID: 'CONSUMER_KEY',
+        clientSecret: 'CONSUMER_SECRET',
+        callbackURL: 'http://localhost:3000/auth/twitter/callback'
+    },
+    github: {
+        clientID: 'APP_ID',
+        clientSecret: 'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/github/callback'
+    },
+    google: {
+        clientID: 'APP_ID',
+        clientSecret: 'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/google/callback'
+    },
+    linkedin: {
+        clientID: 'API_KEY',
+        clientSecret: 'SECRET_KEY',
+        callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+    }
+};
